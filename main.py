248e7c408c7fee449cc185014b813b4a0d5d972f
rparayno <russell.a.parayno@gmail.com>
2023-06-02 06:21:47 +0000

    added css

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': "Housekeeper",
+    'location': "Roxas Isabela",
+    'salary': "P 12,000.00"
+  },
+  {
+    'id': 2,
+    'title': "Cook",
+    'location': "Roxas Isabela",
+    'salary': "P 10,000.00"
+  },
+  {
+    'id': 3,
+    'title': "Security Guard",
+    'location': "Roxas Isabela",
+    'salary': "P 14,000.00"
+  },
+  {
+    'id': 4,
+    'title': "Watcher",
+    'location': "Roxas Isabela",
+    'salary': "P 9,000.00"
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return "Hello, world"
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True )
